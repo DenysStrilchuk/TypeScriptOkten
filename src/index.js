c36b303aa67fb5b515b89e7f839099bd74c8155b
Denys Sttrilchuk <denia876@gmail.com>
2024-01-08 10:51:53 +0200

    Practise with TS Lesson3

--- src/index.js
+++ src/index.js
@@ -1,19 +1,123 @@
-var carServise = {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var carService = {
     getAll: function () { return fetch('http://owu.linkpc.net/carsAPI/v1/cars').then(function (response) { return response.json(); }); },
     create: function (car) { return fetch('http://owu.linkpc.net/carsAPI/v1/cars', {
         method: 'POST',
-        body: JSON.stringify('car'),
-        headers: { 'Content-type': 'aplication/json' }
+        body: JSON.stringify(car),
+        headers: { 'Content-type': 'application/json' }
     }).then(function (response) { return response.json(); }); },
-    deleteById: function (id) { return fetch('http://owu.linkpc.net/carsAPI/v1/cars/${}id'); }
+    deleteById: function (id) { return fetch("http://owu.linkpc.net/carsAPI/v1/cars/".concat(id), {
+        method: 'DELETE'
+    }); }
 };
 var CarRender = /** @class */ (function () {
     function CarRender() {
     }
     CarRender.run = function () {
-        console.log('hello');
+        this._initForm();
+        this._carsShow();
+    };
+    CarRender._carsShow = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var carDiv, cars;
+            var _this = this;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        carDiv = document.querySelector('#carsDiv');
+                        carDiv.innerText = '';
+                        return [4 /*yield*/, carService.getAll()];
+                    case 1:
+                        cars = _a.sent();
+                        cars.forEach(function (car) {
+                            var id = car.id, brand = car.brand, price = car.price, year = car.year;
+                            var itemDiv = document.createElement('div');
+                            itemDiv.innerText = "".concat(id, " ").concat(brand, " -- ").concat(price, " -- ").concat(year);
+                            var btn = document.createElement('button');
+                            btn.innerText = 'delete';
+                            btn.onclick = function () { return __awaiter(_this, void 0, void 0, function () {
+                                return __generator(this, function (_a) {
+                                    switch (_a.label) {
+                                        case 0: return [4 /*yield*/, carService.deleteById(id)];
+                                        case 1:
+                                            _a.sent();
+                                            return [4 /*yield*/, this._carsShow()];
+                                        case 2:
+                                            _a.sent();
+                                            return [2 /*return*/];
+                                    }
+                                });
+                            }); };
+                            itemDiv.appendChild(btn);
+                            carDiv.appendChild(itemDiv);
+                        });
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    CarRender._initForm = function () {
+        var _this = this;
+        var form = document.forms.namedItem('form');
+        form.onsubmit = function (e) { return __awaiter(_this, void 0, void 0, function () {
+            var brand, price, year;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        e.preventDefault();
+                        brand = document.getElementById('brand').value;
+                        price = document.getElementById('price').value;
+                        year = document.getElementById('year').value;
+                        return [4 /*yield*/, carService.create({ brand: brand, price: +price, year: +year })];
+                    case 1:
+                        _a.sent();
+                        return [4 /*yield*/, this._carsShow()];
+                    case 2:
+                        _a.sent();
+                        form.reset();
+                        return [2 /*return*/];
+                }
+            });
+        }); };
     };
     return CarRender;
 }());
-//CarRender.run()
-carServise.getAll().then(function (resolve) { return console.log(resolve); });
+CarRender.run();
+//carService.getAll().then(resolve => console.log(resolve))
+//carService.create({brand:'AstonMartin', price:10500,year:2020}).then(response => console.log(response))
+//carService.deleteById(10087)
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,65 @@
 interface ICar {
-    id: number;
+    id?: number;
     brand: string;
     price: number;
     year: number;
 }
 
-const carServise = {
+const carService = {
     getAll:():Promise<ICar[]> => fetch('http://owu.linkpc.net/carsAPI/v1/cars').then(response => response.json()),
     create:(car:ICar):Promise<ICar> => fetch('http://owu.linkpc.net/carsAPI/v1/cars', {
         method:'POST',
-        body:JSON.stringify('car'),
-        headers:{'Content-type':'aplication/json'}
+        body:JSON.stringify(car),
+        headers:{'Content-type':'application/json'}
     }).then(response => response.json()),
-    deleteById:(id:number):Promise<Response> => fetch('http://owu.linkpc.net/carsAPI/v1/cars/${}id')
+    deleteById:(id:number):Promise<Response> => fetch(`http://owu.linkpc.net/carsAPI/v1/cars/${id}`,{
+    method:'DELETE'
+})
+
 }
 class CarRender {
     static run() :void{
-        console.log('hello');
+        this._initForm()
+        this._carsShow()
     }
+
+    private static async _carsShow():Promise<void>{
+        const carDiv = document.querySelector('#carsDiv')  as HTMLDivElement;
+        carDiv.innerText = ''
+        const cars = await carService.getAll();
+        cars.forEach(car  => {
+            const {id, brand, price, year} = car;
+            const itemDiv = document.createElement('div')
+            itemDiv.innerText = `${id} ${brand} -- ${price} -- ${year}`
+            const btn = document.createElement('button')
+            btn.innerText = 'delete'
+            btn.onclick = async ():Promise<void> => {
+                await carService.deleteById(id)
+                await this._carsShow()
+            }
+            itemDiv.appendChild(btn)
+            carDiv.appendChild(itemDiv)
+        })
+    }
+    private static _initForm(): void {
+        const form = document.forms.namedItem('form') as HTMLFormElement;
+        form.onsubmit = async (e: Event): Promise<void> => {
+            e.preventDefault();
+            const brand = (document.getElementById('brand') as HTMLInputElement).value;
+            const price = (document.getElementById('price') as HTMLInputElement).value;
+            const year = (document.getElementById('year') as HTMLInputElement).value;
+            await carService.create({ brand, price: +price, year: +year });
+            await this._carsShow();
+            form.reset();
+        };
+    }
+
 }
 
-//CarRender.run()
+CarRender.run()
 
-carServise.getAll().then(resolve => console.log(resolve))
+//carService.getAll().then(resolve => console.log(resolve))
+
+//carService.create({brand:'AstonMartin', price:10500,year:2020}).then(response => console.log(response))
+
+//carService.deleteById(10087)
